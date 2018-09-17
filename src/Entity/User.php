178d57b6a3813e7f4e16ee662b7c6f5489e1bd2b
rafaadail <rafaadailph@gmail.com>
2018-09-17 10:17:23 -0300

    Criando a Entity User e implementado a UserInterface

---
+++ src/Entity/User.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="users")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string");
+     */
+    private $username;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string");
+     */
+    private $password;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string");
+     */
+    private $email;
+
+    /**
+     * @var array
+     * @ORM\Column(type="json")
+     */
+    // mysql >= 5.7.8, se não, o type deve ser string e deve ser feito o parse de json_encode e json_decode
+    private $roles;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername ()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return User
+     */
+    public function setUsername ($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword ()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return User
+     */
+    public function setPassword ($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail ()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail ($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles ()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     * @return User
+     */
+    public function setRoles ($roles)
+    {
+        $this->roles[] = $roles;
+        return $this;
+    }
+
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt ()
+    {
+        // caso não estiver usando bcrypt
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials ()
+    {
+        // TODO: Implement eraseCredentials() method.
+        return null;
+    }
+}
