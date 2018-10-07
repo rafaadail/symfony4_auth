6d5f8e9c6c1fe90f02efe0ecbe58c60feb5db234
rafaadail <rafaadailph@gmail.com>
2018-10-07 20:43:39 -0300

    Logon pelo banco de dados.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -45,4 +47,39 @@ class DefaultController extends AbstractController
         return new Response("<html><body><h1>Admin Relatorios Page</h1></body>");
     }
 
+    /**
+     * @param Request $request
+     *
+     * @Route("/insert")
+     * @return Response
+     */
+    public function insert(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setUsername("rafael");
+        $user->setEmail("rafael@email.com");
+        $user->setRoles("ROLE_USER");
+
+        $encoder = $this->get("security.password_encoder");
+        $pass = $encoder->encodePassword($user, "abc");
+        $user->setPassword($pass);
+        $em->persist($user);
+
+        $user = new User();
+        $user->setUsername("admin");
+        $user->setEmail("admin@email.com");
+        $user->setRoles("ROLE_ADMIN");
+
+        $encoder = $this->get("security.password_encoder");
+        $pass = $encoder->encodePassword($user, "123");
+        $user->setPassword($pass);
+        $em->persist($user);
+
+        $em->flush();
+
+        return new Response("<h1>Inserido com sucesso!</h1>");
+
+    }
 }
